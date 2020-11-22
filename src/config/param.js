15961f07bc82e3f1453a1f3b7be563178efbb665
NUO XU <nuox@usc.edu>
2020-11-21 23:52:34 -0800

    stripe frontend

--- src/config/param.js
+++ src/config/param.js
@@ -1,5 +1,5 @@
 module.exports = {
-    URL: "http://localhost:5000/",
-    // URL: "https://flask2interview.azurewebsites.net/",
+    // URL: "http://localhost:5000/",
+    URL: "https://stripe-flask.azurewebsites.net/",
     PUBLIC_KEY:"pk_test_51H73ZHLcoplQnnQXCWnzF3lk7ndlTF2ZBeJEcRDtNjZxgRNUhqULLRTJdgoiibUSmytBBS4ddWsDx7MNgno5HemT00BiEESHiN"
 }
